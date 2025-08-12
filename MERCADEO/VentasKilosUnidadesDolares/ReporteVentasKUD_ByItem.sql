24940488c74f57ec310926b76e056b7a8e681efa
pasadev <pasa.tic.dev@gmail.com>
2025-08-12 09:17:48 -0500

    Cambios Mercadeo, Ventas Kilos Unidades y Dolares

--- MERCADEO/VentasKilosUnidadesDolares/ReporteVentasKUD_ByItem.sql
+++ MERCADEO/VentasKilosUnidadesDolares/ReporteVentasKUD_ByItem.sql
@@ -1,7 +1,8 @@
 -- DROP FUNCTION puntos_venta.reporte_ventas_x_kilo_unidad_dolar(varchar, varchar, varchar, varchar, varchar);
 
+-- drop function puntos_venta.reporte_ventas_x_kilo_unidad_dolar(p_item_inicial varchar, p_item_final varchar, p_fecha_inicial varchar, p_fecha_final varchar);
+
 CREATE OR REPLACE FUNCTION puntos_venta.reporte_ventas_x_kilo_unidad_dolar(
-    p_tipo_reporte varchar,
     p_item_inicial varchar,
     p_item_final varchar,
     p_fecha_inicial varchar,
@@ -11,7 +12,6 @@ CREATE OR REPLACE FUNCTION puntos_venta.reporte_ventas_x_kilo_unidad_dolar(
             (
                 linea               text,
                 descripcion_seccion text,
-                tipo_venta          varchar,
                 unidad_medida       varchar,
                 cantidad            numeric,
                 peso                numeric,
@@ -21,20 +21,9 @@ CREATE OR REPLACE FUNCTION puntos_venta.reporte_ventas_x_kilo_unidad_dolar(
 AS
 $$
 DECLARE
-    v_query          text;
-    v_table_name     text;
-    v_titulo_reporte text;
-    v_linea          text;
+    v_query      text;
+    v_linea      text;
 BEGIN
-    IF p_tipo_reporte = 'PUNTO VENTA' THEN
-        v_table_name := 'puntos_venta.facturas_detalle';
-        v_titulo_reporte := 'Puntos de Venta';
-    ELSIF p_tipo_reporte = 'MAYORISTA' THEN
-        v_table_name := 'cuentas_cobrar.facturas_detalle';
-        v_titulo_reporte := 'Mayorista';
-    ELSE
-        v_titulo_reporte := 'Puntos de Venta + Mayorista';
-    END IF;
 
     v_linea = 'CASE
             WHEN LEFT(df.item, 1) <> ''B'' THEN
@@ -50,22 +39,9 @@ BEGIN
 
     v_query := '
         SELECT
-            ' || v_linea || ' AS linea,
-            CASE
-                WHEN LEFT(df.item, 1) = ''1'' THEN ''Confecciones''
-                WHEN LEFT(df.item, 1) = ''2'' THEN ''Telares''
-                WHEN LEFT(df.item, 1) = ''3'' THEN ''Trenzadoras''
-                WHEN LEFT(df.item, 1) = ''4'' THEN ''Tintoreria''
-                WHEN LEFT(df.item, 1) = ''5'' THEN ''Calcetines''
-                WHEN LEFT(df.item, 1) = ''6'' THEN ''Encajes''
-                WHEN LEFT(df.item, 1) = ''7'' THEN ''Mallas''
-                WHEN LEFT(df.item, 1) = ''9'' THEN ''Seda''
-                WHEN LEFT(df.item, 1) = ''B'' THEN ''Hilos B''
-                WHEN LEFT(df.item, 1) = ''M'' THEN ''Fundas Almacenes''
-                WHEN LEFT(df.item, 1) = ''W'' THEN ''Articulos Promocionales''
-                WHEN LEFT(df.item, 1) = ''Z'' THEN ''Desperdicio''
+            ' || v_linea || ' AS clave,
+
             END AS descripcion_seccion,
-            %s AS tipo_venta,
             r1.unidad_medida,
             (df.cantidad) AS cantidad,
             ROUND((r1.peso * df.cantidad), 3) AS peso,
@@ -81,21 +57,16 @@ BEGIN
             AND LEFT(df.item, 1) != ''Z''';
 
     -- Si es combinación, construimos una consulta con UNION
-    IF p_tipo_reporte NOT IN ('PUNTO VENTA', 'MAYORISTA') THEN
-        v_query := FORMAT(v_query || ' UNION all ' || v_query,
-                          '''Puntos de Venta''::varchar', 'puntos_venta.facturas_detalle',
-                          '''Mayoristas''::varchar', 'cuentas_cobrar.facturas_detalle');
-        v_query := v_query || ' ORDER BY linea ASC, tipo_venta ASC, unidad_medida ASC';
-    ELSE
-        v_query := FORMAT(v_query, '''' || v_titulo_reporte || '''::varchar', v_table_name);
-        v_query := v_query || ' ORDER BY linea ASC, unidad_medida ASC';
-    END IF;
+    v_query := FORMAT(v_query || ' UNION all ' || v_query,
+                      'puntos_venta.facturas_detalle', 'cuentas_cobrar.facturas_detalle');
+    v_query := v_query || ' ORDER BY linea ASC, unidad_medida ASC';
+
     v_query = 'with reporte_ventas as (' || v_query || ')
-        SELECT linea, descripcion_seccion, tipo_venta, unidad_medida, SUM(cantidad) AS cantidad,
+        SELECT linea, descripcion_seccion, unidad_medida, SUM(cantidad) AS cantidad,
                SUM(peso) AS peso, SUM(precio) AS precio
         FROM reporte_ventas
-        group by linea, unidad_medida, descripcion_seccion, tipo_venta
-        order by linea, tipo_venta, unidad_medida
+        group by linea, unidad_medida, descripcion_seccion
+        order by linea, unidad_medida
         ';
 
     RETURN QUERY EXECUTE v_query;
@@ -104,7 +75,6 @@ $$;
 
 SELECT *
 FROM puntos_venta.reporte_ventas_x_kilo_unidad_dolar(
-        'TODO',
         'B',
         'BZ',
         '2025-07-01',
--- MERCADEO/VentasKilosUnidadesDolares/ReporteVentasKiloUnidadDolares.sql
+++ MERCADEO/VentasKilosUnidadesDolares/ReporteVentasKiloUnidadDolares.sql
@@ -1,20 +1,17 @@
-drop function puntos_venta.reporte_ventas_x_kilo_unidad_dolar(p_tipo_reporte varchar,p_item_inicial varchar,p_item_final varchar,p_fecha_inicial varchar,p_fecha_final varchar)
+/*
+DROP FUNCTION puntos_venta.reporte_ventas_x_kilo_unidad_dolar(p_tipo_reporte varchar, p_item_inicial varchar,
+                                                              p_item_final varchar, p_fecha_inicial varchar,
+                                                              p_fecha_final varchar)
+*/
 
-CREATE OR REPLACE FUNCTION puntos_venta.reporte_ventas_x_kilo_unidad_dolar(p_tipo_reporte varchar,
-                                                                           p_item_inicial varchar,
+CREATE OR REPLACE FUNCTION puntos_venta.reporte_ventas_x_kilo_unidad_dolar(p_item_inicial varchar,
                                                                            p_item_final varchar,
                                                                            p_fecha_inicial varchar,
                                                                            p_fecha_final varchar)
     RETURNS TABLE
             (
-                titulo_reporte      text,
-                item_inicial        varchar,
-                item_final          varchar,
-                fecha_inicial       varchar,
-                fecha_final         varchar,
-                primer_digito       text,
+                linea               text,
                 descripcion_seccion text,
-                tipo_venta          varchar,
                 unidad_medida       varchar,
                 cantidad            numeric,
                 peso                numeric,
@@ -24,175 +21,61 @@ CREATE OR REPLACE FUNCTION puntos_venta.reporte_ventas_x_kilo_unidad_dolar(p_tip
 AS
 $function$
 BEGIN
-    IF p_tipo_reporte = 'PUNTO VENTA' THEN
-        RETURN QUERY
-            SELECT 'Puntos de Venta'::text AS                 titulo_reporte,
-                   p_item_inicial::varchar              item_inicial,
-                   p_item_final::varchar                item_final,
-                   p_fecha_inicial::varchar             fecha_inicial,
-                   p_fecha_final::varchar               fecha_final,
-                   LEFT(df.ITEM, 1)                     primer_digito,
-                   CASE
-                       WHEN LEFT(df.ITEM, 1) = '1' THEN 'Confecciones'
-                       WHEN LEFT(df.ITEM, 1) = '2' THEN 'Telares'
-                       WHEN LEFT(df.ITEM, 1) = '3' THEN 'Trenzadoras'
-                       WHEN LEFT(df.ITEM, 1) = '4' THEN 'Tintoreria'
-                       WHEN LEFT(df.ITEM, 1) = '5' THEN 'Calcetines'
-                       WHEN LEFT(df.ITEM, 1) = '6' THEN 'Encajes'
-                       WHEN LEFT(df.ITEM, 1) = '7' THEN 'Mallas'
-                       WHEN LEFT(df.ITEM, 1) = '9' THEN 'Seda'
-                       WHEN LEFT(df.ITEM, 1) = 'B' THEN 'Hilos B'
-                       WHEN LEFT(df.ITEM, 1) = 'M' THEN 'Fundas Almacenes'
-                       WHEN LEFT(df.ITEM, 1) = 'W' THEN 'Articulos Promocionales'
-                       WHEN LEFT(df.ITEM, 1) = 'Z' THEN 'Desperdicio'
-                       END                              descripcion_seccion,
-                   'Punto de Venta'::varchar            tipo_venta,
-                   r1.unidad_medida,
-                   SUM(df.cantidad)                     cantidad,
-                   ROUND(SUM(r1.peso * df.cantidad), 3) peso,
-                   SUM(df.total_precio)                 precio
-            FROM puntos_venta.facturas_detalle df
-                     LEFT JOIN(SELECT i.item          item,
-                                      i.unidad_medida unidad_medida,
-                                      i.peso          peso,
-                                      i.es_vendible   vendible,
-                                      i.es_fabricado  fabricado
-                               FROM control_inventarios.items i) r1 ON df.item = r1.item
-            WHERE (df.item >= p_item_inicial AND df.item <= p_item_final)
-              AND (df.fecha BETWEEN TO_DATE(p_fecha_inicial, 'YYYY-MM-DD') AND TO_DATE(p_fecha_final, 'YYYY-MM-DD'))
-              AND (df.status IS NULL)
-              AND (r1.vendible = TRUE AND r1.fabricado = TRUE)
-              AND (LEFT(df.ITEM, 1) != 'Z')
-            GROUP BY LEFT(df.ITEM, 1), r1.unidad_medida
-            ORDER BY primer_digito ASC, unidad_medida ASC;
+    RETURN QUERY
+        WITH cte_ventas AS (SELECT df.item,
+                                   r1.unidad_medida,
+                                   (df.cantidad)                     cantidad,
+                                   ROUND((r1.peso * df.cantidad), 3) peso,
+                                   (df.total_precio)                 precio
+                            FROM puntos_venta.facturas_detalle df
+                                     LEFT JOIN control_inventarios.items r1 ON df.item = r1.item
+                            WHERE (df.item >= p_item_inicial AND df.item <= p_item_final)
+                              AND (df.fecha BETWEEN TO_DATE(p_fecha_inicial, 'YYYY-MM-DD') AND TO_DATE(p_fecha_final, 'YYYY-MM-DD'))
+                              AND (df.status IS NULL)
+                              AND (r1.es_vendible = TRUE AND r1.es_fabricado = TRUE)
+                              AND (LEFT(df.item, 1) IN ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Z', 'B'))
+                            UNION ALL
+                            SELECT df.item,
+                                   r1.unidad_medida,
+                                   (df.cantidad)                     cantidad,
+                                   ROUND((r1.peso * df.cantidad), 3) peso,
+                                   (df.total_precio)                 precio
+                            FROM cuentas_cobrar.facturas_detalle df
+                                     LEFT JOIN control_inventarios.items r1 ON df.item = r1.item
+                            WHERE (df.item >= p_item_inicial AND df.item <= p_item_final)
+                              AND (df.fecha BETWEEN TO_DATE(p_fecha_inicial, 'YYYY-MM-DD') AND TO_DATE(p_fecha_final, 'YYYY-MM-DD'))
+                              AND (df.status IS NULL)
+                              AND LEFT(df.referencia, 1) NOT IN ('X', 'P')
+                              AND (r1.es_vendible = TRUE AND r1.es_fabricado = TRUE)
+                              AND (LEFT(df.item, 1) IN ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Z', 'B'))),
+             cte_linea AS (SELECT CASE
+                                      WHEN LEFT(df.item, 1) <> 'B' THEN
+                                          CASE
+                                              WHEN LEFT(df.item, 1) = 'D' THEN
+                                                  '1'
+                                              ELSE
+                                                  LEFT(df.item, 1)
+                                              END
+                                      ELSE
+                                          LEFT(df.item, 5)
+                                      END AS clave,
+                                  CASE
+                                      WHEN LEFT(df.item, 1) <> 'B' THEN LEFT(df.item, 1)
+                                      ELSE LEFT(df.item, 5)
+                                      END AS linea,
+                                  *
+                           FROM cte_ventas df)
+        SELECT cl.linea,
+               isc.seccion::text      AS descripcion_seccion,
+               cl.unidad_medida,
+               SUM(cl.cantidad) AS cantidad,
+               SUM(cl.peso)     AS peso,
+               SUM(cl.precio)   AS precio
+        FROM cte_linea cl
+                 LEFT JOIN control_inventarios.item_seccion isc ON LEFT(cl.linea, 1) = isc.codigo
+        GROUP BY cl.linea, cl.unidad_medida, isc.seccion
+        ORDER BY cl.linea, cl.unidad_medida;
 
-    ELSEIF p_tipo_reporte = 'MAYORISTA' THEN
-
-        RETURN QUERY
-            SELECT 'Mayorista'::text AS                       titulo_reporte,
-                   p_item_inicial::varchar              item_inicial,
-                   p_item_final::varchar                item_final,
-                   p_fecha_inicial::varchar             fecha_inicial,
-                   p_fecha_final::varchar               fecha_final,
-                   LEFT(df.ITEM, 1)                     primer_digito,
-                   CASE
-                       WHEN LEFT(df.ITEM, 1) = '1' THEN 'Confecciones'
-                       WHEN LEFT(df.ITEM, 1) = '2' THEN 'Telares'
-                       WHEN LEFT(df.ITEM, 1) = '3' THEN 'Trenzadoras'
-                       WHEN LEFT(df.ITEM, 1) = '4' THEN 'Tintoreria'
-                       WHEN LEFT(df.ITEM, 1) = '5' THEN 'Calcetines'
-                       WHEN LEFT(df.ITEM, 1) = '6' THEN 'Encajes'
-                       WHEN LEFT(df.ITEM, 1) = '7' THEN 'Mallas'
-                       WHEN LEFT(df.ITEM, 1) = '9' THEN 'Seda'
-                       WHEN LEFT(df.ITEM, 1) = 'B' THEN 'Hilos B'
-                       WHEN LEFT(df.ITEM, 1) = 'M' THEN 'Fundas Almacenes'
-                       WHEN LEFT(df.ITEM, 1) = 'W' THEN 'Articulos Promocionales'
-                       WHEN LEFT(df.ITEM, 1) = 'Z' THEN 'Desperdicio'
-                       END                              descripcion_seccion,
-                   'Mayoristas'::varchar                tipo_venta,
-                   r1.unidad_medida,
-                   SUM(df.cantidad)                     cantidad,
-                   ROUND(SUM(r1.peso * df.cantidad), 3) peso,
-                   SUM(df.total_precio)                 precio
-            FROM cuentas_cobrar.facturas_detalle df
-                     LEFT JOIN(SELECT i.item          item,
-                                      i.unidad_medida unidad_medida,
-                                      i.peso          peso,
-                                      i.es_vendible   vendible,
-                                      i.es_fabricado  fabricado
-                               FROM control_inventarios.items i) r1 ON df.item = r1.item
-            WHERE (df.item >= p_item_inicial AND df.item <= p_item_final)
-              AND (df.fecha BETWEEN TO_DATE(p_fecha_inicial, 'YYYY-MM-DD') AND TO_DATE(p_fecha_final, 'YYYY-MM-DD'))
-              AND (df.status IS NULL)
-              AND (r1.vendible = TRUE AND r1.fabricado = TRUE)
-              AND (LEFT(df.ITEM, 1) != 'Z')
-            GROUP BY LEFT(df.ITEM, 1), r1.unidad_medida
-            ORDER BY primer_digito ASC, unidad_medida ASC;
-
-    ELSE -- TODO
-
-        RETURN QUERY
-            SELECT 'Puntos de Venta + Mayorista' AS     titulo_reporte,
-                   p_item_inicial::varchar              item_inicial,
-                   p_item_final::varchar                item_final,
-                   p_fecha_inicial::varchar             fecha_inicial,
-                   p_fecha_final::varchar               fecha_final,
-                   LEFT(df.ITEM, 1)                     primer_digito,
-                   CASE
-                       WHEN LEFT(df.ITEM, 1) = '1' THEN 'Confecciones'
-                       WHEN LEFT(df.ITEM, 1) = '2' THEN 'Telares'
-                       WHEN LEFT(df.ITEM, 1) = '3' THEN 'Trenzadoras'
-                       WHEN LEFT(df.ITEM, 1) = '4' THEN 'Tintoreria'
-                       WHEN LEFT(df.ITEM, 1) = '5' THEN 'Calcetines'
-                       WHEN LEFT(df.ITEM, 1) = '6' THEN 'Encajes'
-                       WHEN LEFT(df.ITEM, 1) = '7' THEN 'Mallas'
-                       WHEN LEFT(df.ITEM, 1) = '9' THEN 'Seda'
-                       WHEN LEFT(df.ITEM, 1) = 'B' THEN 'Hilos B'
-                       WHEN LEFT(df.ITEM, 1) = 'M' THEN 'Fundas Almacenes'
-                       WHEN LEFT(df.ITEM, 1) = 'W' THEN 'Articulos Promocionales'
-                       WHEN LEFT(df.ITEM, 1) = 'Z' THEN 'Desperdicio'
-                       END                              descripcion_seccion,
-                   'Puntos de Venta'::varchar           tipo_venta,
-                   r1.unidad_medida,
-                   SUM(df.cantidad)                     cantidad,
-                   ROUND(SUM(r1.peso * df.cantidad), 3) peso,
-                   SUM(df.total_precio)                 precio
-            FROM puntos_venta.facturas_detalle df
-                     LEFT JOIN(SELECT i.item          item,
-                                      i.unidad_medida unidad_medida,
-                                      i.peso          peso,
-                                      i.es_vendible   vendible,
-                                      i.es_fabricado  fabricado
-                               FROM control_inventarios.items i) r1 ON df.item = r1.item
-            WHERE (df.item >= p_item_inicial AND df.item <= p_item_final)
-              AND (df.fecha BETWEEN TO_DATE(p_fecha_inicial, 'YYYY-MM-DD') AND TO_DATE(p_fecha_final, 'YYYY-MM-DD'))
-              AND (df.status IS NULL)
-              AND (r1.vendible = TRUE AND r1.fabricado = TRUE)
-              AND (LEFT(df.ITEM, 1) != 'Z')
-            GROUP BY LEFT(df.ITEM, 1), r1.unidad_medida
-            UNION
-            SELECT 'Puntos de Venta + Mayorista' AS     titulo_reporte,
-                   p_item_inicial::varchar              item_inicial,
-                   p_item_final::varchar                item_final,
-                   p_fecha_inicial::varchar             fecha_inicial,
-                   p_fecha_final::varchar               fecha_final,
-                   LEFT(df.ITEM, 1)                     primer_digito,
-                   CASE
-                       WHEN LEFT(df.ITEM, 1) = '1' THEN 'Confecciones'
-                       WHEN LEFT(df.ITEM, 1) = '2' THEN 'Telares'
-                       WHEN LEFT(df.ITEM, 1) = '3' THEN 'Trenzadoras'
-                       WHEN LEFT(df.ITEM, 1) = '4' THEN 'Tintoreria'
-                       WHEN LEFT(df.ITEM, 1) = '5' THEN 'Calcetines'
-                       WHEN LEFT(df.ITEM, 1) = '6' THEN 'Encajes'
-                       WHEN LEFT(df.ITEM, 1) = '7' THEN 'Mallas'
-                       WHEN LEFT(df.ITEM, 1) = '9' THEN 'Seda'
-                       WHEN LEFT(df.ITEM, 1) = 'B' THEN 'Hilos B'
-                       WHEN LEFT(df.ITEM, 1) = 'M' THEN 'Fundas Almacenes'
-                       WHEN LEFT(df.ITEM, 1) = 'W' THEN 'Articulos Promocionales'
-                       WHEN LEFT(df.ITEM, 1) = 'Z' THEN 'Desperdicio'
-                       END                              descripcion_seccion,
-                   'Mayoristas'::varchar                tipo_venta,
-                   r1.unidad_medida,
-                   SUM(df.cantidad)                     cantidad,
-                   ROUND(SUM(r1.peso * df.cantidad), 3) peso,
-                   SUM(df.total_precio)                 precio
-            FROM cuentas_cobrar.facturas_detalle df
-                     LEFT JOIN(SELECT i.item          item,
-                                      i.unidad_medida unidad_medida,
-                                      i.peso          peso,
-                                      i.es_vendible   vendible,
-                                      i.es_fabricado  fabricado
-                               FROM control_inventarios.items i) r1 ON df.item = r1.item
-            WHERE (df.item >= p_item_inicial AND df.item <= p_item_final)
-              AND (df.fecha BETWEEN TO_DATE(p_fecha_inicial, 'YYYY-MM-DD') AND TO_DATE(p_fecha_final, 'YYYY-MM-DD'))
-              AND (df.status IS NULL)
-              AND (r1.vendible = TRUE AND r1.fabricado = TRUE)
-              AND (LEFT(df.ITEM, 1) != 'Z')
-            GROUP BY LEFT(df.ITEM, 1), r1.unidad_medida
-            ORDER BY primer_digito ASC, tipo_venta ASC, unidad_medida ASC;
-    END IF;
 END
-
 $function$
 ;
-
